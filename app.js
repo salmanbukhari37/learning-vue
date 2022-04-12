75f436a80935743aefff5d9bbcbfc0d92a00efe4
salmanbukhari-tr <salman.bukhari@technologyrivers.com>
2022-04-12 10:54:27 +0500

    Convert vanilla js to vue

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: '',
 
-function addGoal () {
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
-    listEl.appendChild(listItemEl);
-    inputEl.value = '';
-
-    
-};
-
-buttonEl.addEventListener("click", addGoal);
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = "";
+        }
+    }
+}).mount('#app');
